079d7b53c2cb0ea1237e8fe06444e24402388b77
sohel2178 <sohel.ahmed2178@gmail.com>
2017-09-28 23:43:13 +0600

    hfgh

--- app/src/main/java/com/example/sohel/androidswitchtest/MainActivity.java
+++ app/src/main/java/com/example/sohel/androidswitchtest/MainActivity.java
@@ -3,11 +3,24 @@ package com.example.sohel.androidswitchtest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
 public class MainActivity extends AppCompatActivity {
+
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(getApplicationContext(),
+                "ca-app-pub-5474497946967422~4259843986");
+
+        mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
     }
 }
